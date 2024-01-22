afcaf39ad48bc1559c4c7acd31e45c0fa3c7fce5
estebanana2002 <utti212031@utvco.edu.mx>
2024-01-22 10:54:53 -0600

    Add foodService; reactiveForm finished

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuComponent } from './components/menu/menu.component';
+import { TooltipDirective } from '../core/Directives/Tooltip.directive';
+import { FoodService } from '../services/food.service';
+import { Food } from '../interfaces/Food';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,11 +12,25 @@ import { MenuComponent } from './components/menu/menu.component';
   imports: [
     CommonModule,
     RouterModule,
-    MenuComponent
+    MenuComponent,
+    TooltipDirective
   ],
   // template: `<p>Dashboard works!</p>`
   // styleUrl: './dashboard.component.css',
   templateUrl: `./dashboard.component.html`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class DashboardComponent { }
+export default class DashboardComponent {
+  public userCart: any[] = [];
+  constructor(
+    private FoodS: FoodService
+  ) {
+    this.FoodS.cart$.subscribe(
+      (food: any) => {
+        console.log(food, 'food for observer');
+
+        this.userCart =  food ?? [];
+      }
+    );
+  }
+}
